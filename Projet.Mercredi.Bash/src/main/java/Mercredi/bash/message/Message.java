92baf15f254e82178010edc7aad534efffe2db15
Athar1988 <hakim.athar.88@gmail.com>
2020-06-10 14:30:18 +0100

    nouveau projet configuration

---
+++ Projet.Mercredi.Bash/src/main/java/Mercredi/bash/message/Message.java
@@ -0,0 +1,10 @@
+package Mercredi.bash.message;
+
+public class Message {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("NOtre code ici");
+	}
+
+}
