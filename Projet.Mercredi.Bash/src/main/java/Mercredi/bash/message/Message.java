663fa60e2dfbd72ae7081a93705b7dc02b6833b0
Athar1988 <64782008+Athar1988@users.noreply.github.com>
2020-06-10 15:55:31 +0200

    numéroté les lignes

--- Projet.Mercredi.Bash/src/main/java/Mercredi/bash/message/Message.java
+++ Projet.Mercredi.Bash/src/main/java/Mercredi/bash/message/Message.java
@@ -4,11 +4,11 @@ public class Message {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("NOtre code ici");
-		System.out.println("NOtre code ici");
-		System.out.println("NOtre code ici");
-System.out.println("NOtre code ici");
-		System.out.println("NOtre code ici");
+		System.out.println("NOtre code ici1");
+		System.out.println("NOtre code ici2");
+		System.out.println("NOtre code ici3");
+System.out.println("NOtre code ici4");
+		System.out.println("NOtre code ici5");
 	}
 
 }
